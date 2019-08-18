d0db1233cac51e35bd13879a275d6056e6c1a418
Darek-Mac <darek.baranczuk@gmail.com>
2019-08-18 22:43:56 +0200

    Code refactoring

--- driveservice/driveprovider.py
+++ driveservice/driveprovider.py
@@ -5,36 +5,39 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+class DriveProvider:
 
-def main():
-    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+    def authorize(self):
+        creds = None
+
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
         
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        
+        return creds
 
-    service = build('drive', 'v3', credentials=creds)
+    def listLastModified(self, creds, pageSize=10):
+        service = build('drive', 'v3', credentials=creds)
+        results = service.files().list(pageSize=pageSize, orderBy='modifiedTime desc', fields="nextPageToken, files(id,name,parents)").execute()
+        items = results.get('files', [])
 
-    results = service.files().list(pageSize=2, fields="nextPageToken, files(id,name,parents)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
         for item in items:
             parent = service.files().get(fileId=item['parents'][0]).execute()
             print("{0}\\{1}".format(parent['name'], item['name']))
 
-if __name__ == '__main__':
-    main()
+
+provider = DriveProvider()
+credentials = provider.authorize()
+provider.listLastModified(credentials)
